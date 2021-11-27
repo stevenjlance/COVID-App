c3ebd2f781bfff442d836a3499b64d00a6d2f7ee
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2021-11-27 15:54:56 +0000

    Data updated

---
+++ script.js
@@ -0,0 +1,15 @@
+// Warm up: Log the following for March 7th, 2021
+// Number of total positive tests
+// Number of new positive cases
+// Number of new tests
+// Number currently in the hospital
+// Number of new deaths
+// Total number of deaths as of this date
+
+// Reverse data set
+data = data.reverse();
+
+
+
+
+
